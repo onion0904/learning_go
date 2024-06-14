51914077d4b9a90a1951c3817c8331e40e41f1d7
onion0904 <takerudaze0904@gmain.com>
2024-06-14 19:02:47 +0900

    first commit

---
+++ myapi/main.go
@@ -0,0 +1,20 @@
+package main
+import (
+"log"
+"net/http"
+"github.com/yourname/reponame/handlers"
+"github.com/gorilla/mux"
+)
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+    r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+    r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+    r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+    r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+    r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+// サーバー起動時のログを出力
+log.Println("server start at port 8080")
+// ListenAndServe 関数にて、サーバーを起動
+log.Fatal(http.ListenAndServe(":8080", r))
+}
