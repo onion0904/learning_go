ed782342a399faf11168e02d09fba4537df8bead
onion0904 <takerudaze0904@gmain.com>
2024-07-08 16:01:30 +0900

    finish chapter4

--- myapi/models/data.go
+++ myapi/models/data.go
@@ -23,7 +23,7 @@ var (
 		ID:          1,
 		Title:       "first article",
 		Contents:    "This is the test article.",
-		UserName:    "saki",
+		UserName:    "onion",
 		NiceNum:     1,
 		CommentList: []Comment{Comment1, Comment2},
 		CreatedAt:   time.Now(),
@@ -33,7 +33,7 @@ var (
 		ID:        2,
 		Title:     "second article",
 		Contents:  "This is the test article.",
-		UserName:  "saki",
+		UserName:  "onion",
 		NiceNum:   2,
 		CreatedAt: time.Now(),
 	}
--- myapi/repositories/articles.go
+++ myapi/repositories/articles.go
@@ -1,69 +1,74 @@
-package repository
+package repositories
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
+	"database/sql"
+
 	"github.com/yourname/reponame/models"
 )
+
 const (
 	articleNumPerPage = 5
 )
 
-// 新規投稿をデータベースに insert する関数
-// -> データベースに保存した記事内容と、発生したエラーを返り値にする
+// 新規投稿をDBにinsertする関数
 func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	const sqlStr = `
 	insert into articles (title, contents, username, nice, created_at) values
 	(?, ?, ?, 0, now());
 	`
+
 	var newArticle models.Article
-	newArticle.Title = article.Title
-	newArticle.Contents = article.Contents
-	netwArticle.UserName = article.UserName
+	newArticle.Title, newArticle.Contents, newArticle.UserName = article.Title, article.Contents, article.UserName
+
 	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
-	if err!= nil {
-		fmt.Println(err)
-        return newArticle, err
-    }
+	if err != nil {
+		return models.Article{}, err
+	}
+
 	id, _ := result.LastInsertId()
+
 	newArticle.ID = int(id)
+
 	return newArticle, nil
 }
-// 変数 page で指定されたページに表示する投稿一覧をデータベースから取得する関数
-// -> 取得した記事データと、発生したエラーを返り値にする
+
+// 投稿一覧をDBから取得する関数
 func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
 	const sqlStr = `
 		select article_id, title, contents, username, nice
 		from articles
 		limit ? offset ?;
 	`
+
 	rows, err := db.Query(sqlStr, articleNumPerPage, ((page - 1) * articleNumPerPage))
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	articleArray := make([]models.Article,0)
-	for rows.Next(){
-		var aricle models.Article
-		rows.Scan(&aricle.ID, &aricle.Title, &aricle.Contents,&article.UserName, & article.NiceNum)
-		articleArray = append(articleArray, aricle)
+
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		articleArray = append(articleArray, article)
 	}
+
 	return articleArray, nil
 }
-// 投稿 ID を指定して、記事データを取得する関数
-// -> 取得した記事データと、発生したエラーを返り値にする
+
+// 投稿IDを指定して、記事データを取得する関数
 func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
 	const sqlStr = `
 		select *
 		from articles
 		where article_id = ?;
 	`
-	rows, err := db.Query(sqlStr, articleID)
-	if err!= nil {
-        return models.Article{}, err
-    }
-	defer rows.Close()
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		return models.Article{}, err
+	}
+
 	var article models.Article
 	var createdTime sql.NullTime
 	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
@@ -74,43 +79,44 @@ func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
 	if createdTime.Valid {
 		article.CreatedAt = createdTime.Time
 	}
+
 	return article, nil
 }
-// いいねの数を update する関数
-// -> 発生したエラーを返り値にする
+
+// いいねの数をupdateする関数
 func UpdateNiceNum(db *sql.DB, articleID int) error {
 	tx, err := db.Begin()
-	if err!= nil {
-        return err
-    }
-	
+	if err != nil {
+		return err
+	}
+
 	const sqlGetNice = `
 		select nice
 		from articles
 		where article_id = ?;
-	`	
-	row, err := tx.QueryRow(sqlGetNice, articleID)
+	`
+	row := tx.QueryRow(sqlGetNice, articleID)
 	if err := row.Err(); err != nil {
 		tx.Rollback()
 		return err
 	}
 
-	var niceNum int
-	err = row.Scan(&niceNum)
-	if err!= nil {
-        tx.Rollback()
-        return err
-    }
-	const sqlUpdateNice = `update articles set nice = ? where article_id = ?`
-
-	_, err = tx.Exec(sqlUpdateNice, niceNum + 1, articleID)
-	if err!= nil {
-        tx.Rollback()
-        return err
-    }
-	if err := tx.Commit(); err!= nil {
+	var nicenum int
+	err = row.Scan(&nicenum)
+	if err != nil {
+		tx.Rollback()
 		return err
-    }
+	}
 
+	const sqlUpdateNice = `update articles set nice = ? where article_id = ?`
+	_, err = tx.Exec(sqlUpdateNice, nicenum+1, articleID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
 	return nil
 }
