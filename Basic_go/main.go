c6e4c1f03f7a9e4a1704840bda36faf722dca210
onion0904 <takerudaze0904@gmain.com>
2024-09-26 15:36:59 +0900

    add basic

---
+++ Basic_go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	// "github.com/joho/godotenv"
+	// "go-basics/calculator"
+)
+
+type Task struct {
+	Title string
+	Estimate int
+}
+
+func funcd(files []string) []string {
+	f, err := os.Open(name)
+}
+
+func main() {
+	// godotenv.Load()
+	// fmt.Println(os.Getenv("GO_ENV"))
+	
+}
+
+
